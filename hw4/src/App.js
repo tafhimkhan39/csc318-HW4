3e0925c867143ad0427d635f1c1a8e11a1a7e659
Tafhim Khan <tafhimkhan@Tafhims-MacBook-Pro-2.local>
2023-07-21 01:06:06 -0400

    initial change

--- hw4/src/App.js
+++ hw4/src/App.js
@@ -4,96 +4,98 @@ import './App.css';
 function App() {
   const [displayValue, setDisplayValue] = useState('0');
 
-
-  switch (btnValue) {
-    case 'x!':
-      try {
-        display.value = factorial((display.value));
-      } catch {
-        display.value = 'Error';
-      }
-      break;
-    case '%':
-      try {
-        display.value = (display.value) / 100;
-      } catch {
-        display.value = 'Error';
-      }
-      break;
-    case 'AC':
-      display.value = '';
-      break;
-    case 'sin':
-      try {
-        display.value = Math.sin((display.value * Math.PI) / 180);
+  // Implement button functionality
+  const buttonClick = (buttonText) => {
+    switch (btnValue) {
+      case 'x!':
+        try {
+          display.value = factorial((display.value));
         } catch {
-        display.value = 'Error';
+          display.value = 'Error';
         }
-      break;
-    case 'ln':
-      try {
-        display.value = Math.log((display.value));
+        break;
+      case '%':
+        try {
+          display.value = (display.value) / 100;
         } catch {
-        display.value = 'Error';
+          display.value = 'Error';
         }
-      break;
-    case 'cos':
-      try {
-        display.value = Math.cos((display.value * Math.PI) / 180);
+        break;
+      case 'AC':
+        display.value = '';
+        break;
+      case 'sin':
+        try {
+          display.value = Math.sin((display.value * Math.PI) / 180);
+          } catch {
+          display.value = 'Error';
+          }
+        break;
+      case 'ln':
+        try {
+          display.value = Math.log((display.value));
+          } catch {
+          display.value = 'Error';
+          }
+        break;
+      case 'cos':
+        try {
+          display.value = Math.cos((display.value * Math.PI) / 180);
+          } catch {
+          display.value = 'Error';
+          }
+        break;
+      case 'log':
+        try {
+          display.value = Math.log10((display.value));
+          } catch {
+          display.value = 'Error';
+          }
+        break;
+      case 'tan':
+        try {
+          display.value = Math.tan((display.value * Math.PI) / 180);
         } catch {
-        display.value = 'Error';
+          display.value = 'Error';
         }
-      break;
-    case 'log':
-      try {
-        display.value = Math.log10((display.value));
+        break;
+      case '√':
+        try {
+          display.value = Math.sqrt((display.value));
         } catch {
-        display.value = 'Error';
+          display.value = 'Error';
         }
-      break;
-    case 'tan':
-      try {
-        display.value = Math.tan((display.value * Math.PI) / 180);
-      } catch {
-        display.value = 'Error';
-      }
-      break;
-    case '√':
-      try {
-        display.value = Math.sqrt((display.value));
-      } catch {
-        display.value = 'Error';
-      }
-      break;
-    case 'EXP':
-      try {
-        display.value = Math.exp((display.value));
-      } catch {
-        display.value = 'Error';
-      }
-      break;
-    case 'x^y':
-      try {
-        const base = display.value;
-        const exponent = display.value;
-        display.value = Math.pow((base), (exponent));
-      } catch {
-        display.value = 'Error';
-      }
-      break;
-    case '=':
-      try {
-        display.value = evalExpression(display.value);
+        break;
+      case 'EXP':
+        try {
+          display.value = Math.exp((display.value));
         } catch {
-        display.value = 'Error';
+          display.value = 'Error';
         }
-      break;
-    default:
-      if (display.value === 'Error') {
-        display.value = btnValue;
-      } else {
-        display.value += btnValue;
-      }
+        break;
+      case 'x^y':
+        try {
+          const base = display.value;
+          const exponent = display.value;
+          display.value = Math.pow((base), (exponent));
+        } catch {
+          display.value = 'Error';
+        }
+        break;
+      case '=':
+        try {
+          display.value = evalExpression(display.value);
+          } catch {
+          display.value = 'Error';
+          }
+        break;
+      default:
+        if (display.value === 'Error') {
+          display.value = btnValue;
+        } else {
+          display.value += btnValue;
+        }
+    }
   };
 
   function factorial(n) {
@@ -102,34 +104,34 @@ function App() {
     } else {
       return n * factorial(n - 1);
     }
-};
+}
 
 function evalExpression(expression) {
-      expression = expression.replace(/x/g, '*').replace(/÷/g, '/');
-      expression = expression.replace(/(-?\d+)([+\-*\/])(-?\d+)/g, (_, num1, operator, num2) => {
-        num1 = parseFloat(num1);
-        num2 = parseFloat(num2);
-        switch (operator) {
-          case '/':
-            return num1 / num2;
-          case '*':
-            return num1 * num2;
-          case '-':
-            return num1 - num2;
-          case '+':
-            return num1 + num2;
-        }
-      });
+  expression = expression.replace(/x/g, '*').replace(/÷/g, '/');
+  expression = expression.replace(/(-?\d+)([+\-*\/])(-?\d+)/g, (_, num1, operator, num2) => {
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+    switch (operator) {
+      case '/':
+        return num1 / num2;
+      case '*':
+        return num1 * num2;
+      case '-':
+        return num1 - num2;
+      case '+':
+        return num1 + num2;
+    }
+  });
 
 
 
-  let result = eval(expression); 
+let result = eval(expression); 
 
-  return result;
-};
+return result;
+}
 
-return (
-  <div class="container">
+  return (
+    <div class="container">
         
         <input type="text" class="bar" id="display" placeholder="0"></input>
 
@@ -171,7 +173,8 @@ return (
             <button >+</button>
         </div>
     </div>
-);
+  );
 }
+  
 
 export default App;
